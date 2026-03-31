c83edf4810ab3b8d60096b98641a6351f59484db
JessimLaib.lua <j.laib@eleve.leschartreux.net>
2026-03-31 17:22:20 +0200

    Refacto PayementStrategy

--- src/main/java/fr/horizion/savoir/core/modules/payement/PayementStrategy.java
+++ src/main/java/fr/horizion/savoir/core/modules/payement/PayementStrategy.java
@@ -2,10 +2,5 @@ package fr.horizion.savoir.core.modules.payement;
 
 public interface PayementStrategy {
 
-    private boolean pay(float amount) {
-
-        return false;
-    }
-
     boolean pay(float amount);
 }
