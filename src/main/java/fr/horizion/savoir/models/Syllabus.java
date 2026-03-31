0a23cfa0f7a84bc91a0a0f2e1da4976e4035154d
JessimLaib.lua <j.laib@eleve.leschartreux.net>
2026-03-31 17:37:58 +0200

    Add Syllabus Model

--- src/main/java/fr/horizion/savoir/models/Syllabus.java
+++ src/main/java/fr/horizion/savoir/models/Syllabus.java
@@ -1,4 +1,43 @@
 package fr.horizion.savoir.models;
 
+import java.util.List;
+
 public class Syllabus {
+
+    private int id;
+    private List<String> chapitres;
+    private List<String> objectifs;
+
+    public Syllabus(int id, List<String> chapitres, List<String> objectifs) {
+        this.id = id;
+        this.chapitres = chapitres;
+        this.objectifs = objectifs;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<String> getChapitres() {
+        return chapitres;
+    }
+    public List<String> getObjectifs() {
+        return objectifs;
+    }
+
+    public void setChapitres(List<String> chapitres) {
+        this.chapitres = chapitres;
+    }
+
+    public void setObjectifs(List<String> objectifs) {
+        this.objectifs = objectifs;
+    }
+
+    public void addChapitre(String chapitre) {
+        this.chapitres.add(chapitre);
+    }
+
+    public void addObjectif(String objectif) {
+        this.objectifs.add(objectif);
+    }
 }
