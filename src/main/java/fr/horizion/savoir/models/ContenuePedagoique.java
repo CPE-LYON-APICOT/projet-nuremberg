f85fea292a4d11ce632dc7f34937ef6f3dc62dfb
JessimLaib.lua <j.laib@eleve.leschartreux.net>
2026-03-31 16:44:07 +0200

    ContenuePedagoique Model

--- src/main/java/fr/horizion/savoir/models/ContenuePedagoique.java
+++ src/main/java/fr/horizion/savoir/models/ContenuePedagoique.java
@@ -1,4 +1,17 @@
 package fr.horizion.savoir.models;
 
-public class ContenuePedagoique {
+public class ContenuePedagoique  {
+
+    private String titre;
+    private Boolean estTermine;
+
+    public ContenuePedagoique(String titre, Boolean estTermine) {
+        this.titre = titre;
+        this.estTermine = estTermine;
+    }
+
+
+    public void affiche() {
+        System.out.println(titre);
+    }
 }
