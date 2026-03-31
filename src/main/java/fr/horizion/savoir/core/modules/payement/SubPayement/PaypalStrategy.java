9df26c89d8ca385db6bcbe3bfe34c17044d2ce08
JessimLaib.lua <j.laib@eleve.leschartreux.net>
2026-03-31 16:41:31 +0200

    Payement Strategy init

--- src/main/java/fr/horizion/savoir/core/modules/payement/SubPayement/PaypalStrategy.java
+++ src/main/java/fr/horizion/savoir/core/modules/payement/SubPayement/PaypalStrategy.java
@@ -1,6 +1,13 @@
 package fr.horizion.savoir.core.modules.payement.SubPayement;
 
-abstract public class PaypalStrategy {
+import fr.horizion.savoir.core.modules.payement.PayementStrategy;
+
+abstract public class PaypalStrategy implements PayementStrategy {
+
+    @Override
+    public boolean pay(float amount) {
+        return false;
+    }
 
 
 }
