9dfad2a46c6e88bfeac80ea6f33267b4ad75347b
JessimLaib.lua <j.laib@eleve.leschartreux.net>
2026-03-31 17:24:15 +0200

    Init PayementProcessor

--- src/main/java/fr/horizion/savoir/models/PaymentProcessor.java
+++ src/main/java/fr/horizion/savoir/models/PaymentProcessor.java
@@ -1,4 +1,20 @@
 package fr.horizion.savoir.models;
 
-public class Payement {
+import fr.horizion.savoir.core.modules.payement.PayementStrategy;
+
+public class PaymentProcessor {
+
+
+    private PayementStrategy payementStrategy;
+
+     public PaymentProcessor(PayementStrategy payementStrategy) {
+         this.payementStrategy = payementStrategy;
+     }
+
+     public boolean process(float amount) {
+         return payementStrategy.pay(amount);
+     }
+
+
+
 }
