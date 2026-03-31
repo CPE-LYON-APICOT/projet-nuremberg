dd09ade901155a8fb7e681ef88d05d58333297e2
JessimLaib.lua <j.laib@eleve.leschartreux.net>
2026-03-31 16:42:08 +0200

    Fromation Model

--- src/main/java/fr/horizion/savoir/models/Formartion.java
+++ src/main/java/fr/horizion/savoir/models/Formartion.java
@@ -1,4 +1,54 @@
 package fr.horizion.savoir.models;
 
+import java.util.List;
+import java.util.Observer;
+
 public class Formartion {
+
+    private int id;
+    private String titre;
+    private String description;
+    private float prix;
+    private List<Observer> lesEleves;
+
+    public Formartion(int id, String titre, String description, float prix, List<Observer> lesEleves) {
+        this.id = id;
+        this.titre = titre;
+        this.description = description;
+        this.prix = prix;
+        this.lesEleves = lesEleves;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+
+    public List<Observer> getLesEleves() {
+        return lesEleves;
+    }
 }
