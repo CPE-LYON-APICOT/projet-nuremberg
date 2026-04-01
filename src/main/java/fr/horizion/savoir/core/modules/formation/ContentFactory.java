a83e4dce85f6b5d53c421347ee03cad191247abc
JessimLaib.lua <j.laib@eleve.leschartreux.net>
2026-04-01 16:23:34 +0200

    Formation Implement

--- src/main/java/fr/horizion/savoir/core/modules/formation/ContentFactory.java
+++ src/main/java/fr/horizion/savoir/core/modules/formation/ContentFactory.java
@@ -1,4 +1,27 @@
 package fr.horizion.savoir.core.modules.formation;
 
+import fr.horizion.savoir.models.formation.ContenuePedagogique;
+
 public class ContentFactory {
+
+
+    public ContenuePedagogique createContent(String type, String titre, Boolean estTermine, int specificVal ) {
+
+        return switch (type) {
+            case "video" -> {
+                yield new Video(titre, estTermine, specificVal);
+
+
+            }
+
+            case "quiz" -> {
+                yield new Quiz( titre, estTermine, specificVal);
+            }
+
+            case "pdf" -> {
+                yield new PDF( titre, estTermine, specificVal);
+            }
+            default -> null;
+        };
+    }
 }
