3de149461b2277f16fc5067e360faa90368b790b
JessimLaib.lua <j.laib@eleve.leschartreux.net>
2026-04-07 11:24:02 +0200

    Add Observers

--- src/main/java/fr/horizion/savoir/shared/observer/Subject.java
+++ src/main/java/fr/horizion/savoir/shared/observer/Subject.java
@@ -1,4 +1,8 @@
 package fr.horizion.savoir.shared.observer;
 
 public interface Subject {
+
+    void attach(Observer observer);
+    void detach(Observer observer);
+    void notify(String message);
 }
