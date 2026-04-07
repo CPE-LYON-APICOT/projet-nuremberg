799c779ed3fa67ae3fc8c3cb689b7a3c069c7070
JessimLaib.lua <j.laib@eleve.leschartreux.net>
2026-04-07 14:43:15 +0200

    Formation & Promotion Decorator

--- src/main/java/fr/horizion/savoir/core/modules/promotion/PromotionDecorator.java
+++ src/main/java/fr/horizion/savoir/core/modules/promotion/PromotionDecorator.java
@@ -1,4 +1,23 @@
 package fr.horizion.savoir.core.modules.promotion;
 
-public class PromotionDecorator {
+import fr.horizion.savoir.models.formation.Formartion;
+
+public class PromotionDecorator extends FormationDecorator {
+
+    private float remise;
+
+    public PromotionDecorator(Formartion wrappedFormation, float remise) {
+        super(wrappedFormation);
+        this.remise = remise;
+    }
+
+    @Override
+    public float getPrice() {
+        return wrappedFormation.getPrix() - (wrappedFormation.getPrix() * remise / 100);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (Remise: " + remise + "%)";
+    }
 }
